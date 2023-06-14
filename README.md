5401baaf02a7a07f37073ff34af96d965800c867
Khizar Iqbal <khizar.i0066@gmail.com>
2023-06-14 21:33:01 +0100

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + Shadcn + Vite Skeleton
+
+1. Clone repo
+2. `cd` into the directory and run `npm i`
+3. To install a shadcn component, USE THE MANUAL INSTRUCTIONS!!
+4. You're welcome
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Switch } from "./components/Switch";
+
+function App() {
+	return (
+		<>
+			<div className=" m-auto w-3/6 p-10 text-center">
+				<h1 className="text-3xl font-bold">React+shadcn+vite</h1>
+				<Switch />
+			</div>
+		</>
+	);
+}
+
+export default App;
